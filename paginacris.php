6e9f84c161b076388b9de8db051ec0c11a7dbe65
josuetelles <josumam34@gmail.com>
2023-11-07 17:54:34 -0600

    Commit de Josue, varios cambios. xd

--- paginacris.php
+++ paginacris.php
@@ -14,40 +14,39 @@
    <div class="contenedor">
         <div class="columna">
             <div class="media">
-                <img src="R.jpg" alt="Imagen 1">
+                <img src="media/R.jpg" alt="Imagen 1" width="300" height="300">
                 <h2>Chems Frances</h2>
             </div>
 
             <div class="media">
-                <img src="imagen2.jpg" alt="Imagen 2">
+                <img src="media/R2.jpg" alt="Imagen 2" width="300" height="300">
                 <h2>Chems rana</h2>
             </div>
 
             <div class="media">
-                <img src="imagen3.jpg" alt="Imagen 3">
+                <img src="media/R3.jpg" alt="Imagen 3" width="300" height="300">
                 <h2>Chems gamer</h2>
             </div>
         </div>
 
         <div class="columna">
             <div class="media">
-            <img class="media" src="media/img/R.jpg" alt="">
+            <img class="media" src="media/R4.jpg" alt="" width="300" height="300">
                 <h2>Chems ranchero</h2>
             </div>
 
             <div class="media">
-                <img src="imagen5.jpg" alt="Imagen 5">
+                <img src="media/R5.jpg" alt="Imagen 5" width="300" height="300">
                 <h2>Chems ingeniero</h2>
             </div>
 
             <div class="media">
-                <img src="imagen6.jpg" alt="Imagen 6">
+                <img src="media/R6.jpg" alt="Imagen 6" width="300" height="300">
                 <h2>Chems Otaku</h2>
             </div>
         </div>
     </div>
 
-    <a href="index.php"><button>Volver al inicio</button></a>
     <a href="index.php"><button>Volver al inicio</button></a>
 </body>
 </html>
--- paginajosue.php
+++ paginajosue.php
@@ -3,13 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina de Josue</title>
+    <title>Pagina de Josue xd</title>
     <link rel="stylesheet"
     type="text/css"
     href="stylo.css">
 </head>
 <body>
+    <section>
+        <h1>Hola esta es mi página web. xd</h1>
+    </section>
+    <section>
+        <h2> Top personal de mis videojuegos favoritos </h2>
+    </section>
+    <section>
+        <div>
+            <h3>Top 5 Gears of war</h3>
+            <img src="media/gears.jpg" alt="Gears of war augurio" width="400" height="300">
+        </div>
+        <div>
+            <h3>Top 4 Alice Madness returns</h3>
+            <img src="media/alice.jpg" alt="Alice Madness Returns"width="400" height="500">
+        </div>
+        <div>
+            <h3>Top 3 Fire Emblem</h3>
+            <img src="media/fire.jpg" alt="Fire Emplem The bibding blade" width="350" height="600">
+        </div>
+        <div>
+            <h3>Top 2 League of legends</h3>
+            <img src="media/lol.jpg" alt="Ornn dios de la forja" width="400" height="300">
+        </div>
+        <div>
+            <h3>Top 1 Halo</h3>
+            <img src="media/halo.jpg" alt="Halo" width="400" height="300">
+        </div>
+    </section>
     
+
     <a href="index.php"><button>Volver al inicio</button></a>
 </body>
 </html>
